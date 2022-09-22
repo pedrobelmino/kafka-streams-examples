8439d019700e63b20d245eae3e5b0297e06831fa
Pedro Belmino Chaves Filho <pedrobelmino@gmail.com>
2022-09-22 20:33:05 -0300

    modificado o exemplo para tentativas de autorização

--- src/main/java/br/com/pedrobelmino/examples/streams/AuthorizationAttemptsLambdaExample.java
+++ src/main/java/br/com/pedrobelmino/examples/streams/AuthorizationAttemptsLambdaExample.java
@@ -1,22 +1,19 @@
 package br.com.pedrobelmino.examples.streams;
 
-import io.confluent.common.utils.TestUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
 
-import java.util.Arrays;
 import java.util.Properties;
-import java.util.regex.Pattern;
 
-public class WordCountLambdaExample {
+@Slf4j
+public class AuthorizationAttemptsLambdaExample {
 
-    static final String inputTopic = "streams-plaintext-input";
-    static final String outputTopic = "streams-wordcount-output";
+    static final String INPUT_TOPIC = "authorization-attempts-input";
+    static final String OUTPUT_TOPIC = "authorization-attempts-count-output";
 
     public static void main(final String[] args) {
         final String bootstrapServers = args.length > 0 ? args[0] : "localhost:9094";
@@ -24,7 +21,7 @@ public class WordCountLambdaExample {
         final Properties streamsConfiguration = getStreamsConfiguration(bootstrapServers);
 
         final StreamsBuilder builder = new StreamsBuilder();
-        createWordCountStream(builder);
+        createStream(builder);
         final KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
         streams.cleanUp();
         streams.start();
@@ -33,28 +30,25 @@ public class WordCountLambdaExample {
 
     static Properties getStreamsConfiguration(final String bootstrapServers) {
         final Properties streamsConfiguration = new Properties();
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-example");
-        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "wordcount-lambda-example-client");
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "authorization-attempts-example");
+        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "authorization-attempts-example-client");
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
         streamsConfiguration.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, TestUtils.tempDirectory().getAbsolutePath());
+        streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, "c:\\temp\\kafka-state");
         return streamsConfiguration;
     }
 
 
-    static void createWordCountStream(final StreamsBuilder builder) {
-        final KStream<String, String> textLines = builder.stream(inputTopic);
-        final Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
-
-        final KTable<String, Long> wordCounts = textLines
-                .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
-                .groupBy((keyIgnored, word) -> word)
-                .count();
-
-        wordCounts.toStream().to(outputTopic, Produced.with(Serdes.String(), Serdes.Long()));
+    static void createStream(final StreamsBuilder builder) {
+        builder.stream(INPUT_TOPIC)
+                .peek((key, value) -> log.info("key {} value {}", key, value))
+                .groupBy((key, value) -> (String)value)
+                .count()
+                .toStream()
+                .to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
     }
 
 }
